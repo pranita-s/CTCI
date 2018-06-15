0156ab45951b5686b4be1b1f6a9998507e9307d9
Pranita Sharma <pranitarsharma@gmail.com>
2018-06-15 10:21:08 +0530

    1.7

--- Arrays and Strings/RotateMatrix.py
+++ Arrays and Strings/RotateMatrix.py
@@ -5,7 +5,7 @@
 
 def rotateMatrix(mat):
   mat_size = len(mat)-1
-  for i in range(mat_size//2):
+  for i in range(len(mat)//2):
     for j in range(i, mat_size - i):
       mat[i][j] , mat[~j][i] , mat[~i][~j], mat[j][~i]  = mat[~j][i] , mat[~i][~j] , mat[j][~i], mat[i][j]
 
