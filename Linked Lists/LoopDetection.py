664eee4d611b5435521f21b676e89f5fcd99308f
Pranita Sharma <pranitarsharma@gmail.com>
2018-06-16 00:47:11 +0530

    2.8

--- Linked Lists/LoopDetection.py
+++ Linked Lists/LoopDetection.py
@@ -4,12 +4,14 @@
 
 def loopDetection(head):
 	slow = fast = head
-	while slow is not fast:
-		slow, fast  = slow.next, fast.next.next
 	
-	slow = head
-	
-	while slow is not fast:
-		slow, fast = slow.next, fast.next
-	
-	return slow
+	while fast and fast.next and fast.next.next:
+		slow, fast = slow.next, fast.next.next
+		
+		if slow is fast:
+			slow = head
+			
+			while slow is not fast:
+				slow, fast = slow.next, fast.next.next
+			return slow
+	return None
