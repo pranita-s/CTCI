8e51dd623f2ce5023c2f6c526bd0bf8f9f179c2e
Pranita Sharma <pranitarsharma@gmail.com>
2018-06-15 10:20:30 +0530

    1.7

---
+++ Arrays and Strings/RotateMatrix.py
@@ -0,0 +1,13 @@
+
+# TIME - O(m*n)
+# SPACE - O(1)
+
+
+def rotateMatrix(mat):
+  mat_size = len(mat)-1
+  for i in range(mat_size//2):
+    for j in range(i, mat_size - i):
+      mat[i][j] , mat[~j][i] , mat[~i][~j], mat[j][~i]  = mat[~j][i] , mat[~i][~j] , mat[j][~i], mat[i][j]
+
+  return mat
+  
