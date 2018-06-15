2528ed0e5c939d2570de013cd7ce218fc336ea1d
Pranita Sharma <pranitarsharma@gmail.com>
2018-06-16 00:24:25 +0530

    2.6

--- Linked Lists/Palindrome.py
+++ Linked Lists/Palindrome.py
@@ -1,26 +1,25 @@
 
-# TIME - 
-# SPACE -
+# TIME - O(n)
+# SPACE - O(1)
 
 def reverse(l):
 	dummy = ListNode(0)
 	dummy.next = l
 	dummy = sublist_head
 	subiter = sublist_head.next
-	while subiter:
+	while subiter.next:
 		temp = subiter.next
 		sublist_head.next, subiter.next, temp.next  = temp, temp.next, sublist_head.next
 	return dummy.next
 		
 
 def Palindrome(head):
-	size = findSize(head)
 	slow = fast = head
 	
 	while fast and fast.next:
 		slow , fast = slow.next, fast.next.next
 	
-	if size % 2:
+	if fast:
 		slow = slow.next
 	
 	l1, l2 = head, reverse(slow)
@@ -29,11 +28,35 @@ def Palindrome(head):
 		if l1.data != l2.data:
 			return False
 		l1, l2 = l1.next, l2.next
-	
-	if l1 or l2:
-		return False
-	
 	return True
+	
+
+# TIME - O(n)
+# SPACE - O(n)
+
+def palindrome(head):
+	s = []
+	slow = fast = head
+	while fast and fast.next:
+		s.append(slow.data)
+		slow , fast = slow.next, fast.next.next
+		
+	if fast:
+		slow = slow.next
+		
+	while s:
+		if slow.data != s.pop():
+			return False
+		slow = slow.next
+	return True
+	
+	
+	
+	
+	
+		
+	
+	
 	
 	
 	
