f0fb63c9449d27eb1756d3d3c75509a992949d69
Pranita Sharma <pranitarsharma@gmail.com>
2018-06-16 00:34:39 +0530

    2.7

--- Linked Lists/Intersection.py
+++ Linked Lists/Intersection.py
@@ -2,6 +2,13 @@
 # TIME - O(n)
 # SPACE - O(1)
 
+def findSize(head):
+	size = 0
+	while head:
+		size += 1
+		head = head.next
+	return size
+
 def findIntersection(l1,l2):
 	size1, size2 = findSize(l1), findSize(l2)
 	
@@ -11,7 +18,7 @@ def findIntersection(l1,l2):
 	for _ in range(size1 - size2):
 		l1 = l1.next
 	
-	while l1 != l2:
+	while l1 and l2 and l1 is not l2:
 		l1, l2 = l1.next, l2.next
 	
 	return l1
