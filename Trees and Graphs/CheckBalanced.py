f3ba48141e4def9ce181798f649e18b73bf70978
Pranita Sharma <pranitarsharma@gmail.com>
2018-06-17 12:09:43 +0530

    4.4

---
+++ Trees and Graphs/CheckBalanced.py
@@ -0,0 +1,16 @@
+
+# TIME - O(n)
+# SPACE - O(1)
+
+def checkBalanced(root):
+	if not root:
+		return True
+		
+	def helper(root):
+		if not root:
+			return 0
+		left_height , right_height = helper(root.left), helper(root.right)		
+		if left_height < 0 or right_height < 0 or abs(left_height,right_height) > 1:
+			return -1
+		return max(left_height,right_height)+1	
+	return (helper(root) >= 0)
