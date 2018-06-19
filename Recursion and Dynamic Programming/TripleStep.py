c4a1b6a02dfa90ece7c29d634f623a01dc9228e1
Pranita Sharma <pranitarsharma@gmail.com>
2018-06-20 00:34:39 +0530

    8.1

--- Recursion and Dynamic Programming/TripleStep.py
+++ Recursion and Dynamic Programming/TripleStep.py
@@ -3,9 +3,16 @@
 # SPACE - O(n)
 
 def tripleStep(n):
+	if n == 1:
+		return 1
+	if n == 2:
+		return 2
+	if n == 3:
+		return 4
 	dp = [0] * (n+1)
 	dp[1] = 1
 	dp[2] = 2
-	for i in range(3,n+1):
+	dp[3] = 4
+	for i in range(4,n+1):
 		dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
 	return dp[-1]
