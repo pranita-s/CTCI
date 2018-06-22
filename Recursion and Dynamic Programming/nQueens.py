eed820b0b384da0a270513a8b2b09066f6579e02
Pranita Sharma <pranitarsharma@gmail.com>
2018-06-22 09:12:36 +0530

    8.12

---
+++ Recursion and Dynamic Programming/nQueens.py
@@ -0,0 +1,19 @@
+# TIME - O(n!)
+
+def nQueens(n):
+  def queenPlacement(row):
+    
+    if row ==n:
+      result.append(col_placement)
+      return    
+    for col in range(n):      
+      if all(abs(col-c) not in (0,row-i) for i,c in enumerate(col_placment[:row])):
+        col_placement[row] = col
+        queenPlacement(row+1)
+        
+  result = []
+  col_placement = [0] * n
+  queenPlacement(0)
+  return result
+ 
+print(nQueens(4))
