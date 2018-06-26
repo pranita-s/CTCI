c460ecd8b0800385b9eea73d3e5700266b4f9353
Pranita Sharma <pranitarsharma@gmail.com>
2018-06-26 08:42:14 +0530

    16.1

---
+++ Moderate/NumberSwap.py
@@ -0,0 +1,18 @@
+
+
+
+def numberSwapper(x,y):
+  
+  x = x + y
+  y = x - y
+  x = x - y
+  
+  return (x,y)
+  
+def numberSwapper(x,y):
+  
+  x = x ^ y
+  y = x ^ y
+  x = x ^ y
+  
+  return (x,y)
